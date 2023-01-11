09ba2cd3f270adc0b14bdf45ef6c0914b8246c20
emekeo <gistwithcollins@gmail.com>
2023-01-11 00:55:14 -0800

    Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-# Creates a file
+# create a file in /tmp.
+
 file { '/tmp/school':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
